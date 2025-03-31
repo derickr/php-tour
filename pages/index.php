5688ef56396f3198877ffe46a15ffcdb452d4714
Derick Rethans <github@derickrethans.nl>
2025-03-31 17:08:21 +0100

    Commit regenerated files

--- pages/index.php
+++ pages/index.php
@@ -1,27 +1,30 @@
 <?php
-
-$index = [
-	0 => [
-		'title' => "Welcome",
-		'children' => [
-			0 => [ 'title' => 'Hello Earthling', ]
-		],
-	],
-	1 => [
-		'title' => "Variables",
-		'children' => [
-			0 => [ 'title' => 'Assignment', ],
-			1 => [ 'title' => 'Display', ],
-			2 => [ 'title' => 'Type Juggling', ],
-		],
-	],
-	2 => [
-		'title' => "Functions",
-		'children' => [
-			0 => [ 'title' => 'Declaration' ],
-		],
-	],
-];
-
-return $index;
+return array (
+  0 => 
+  array (
+    'title' => 'Welcome',
+    'children' => 
+    array (
+      0 => 
+      array (
+        'title' => 'Hello Earthling',
+      ),
+    ),
+  ),
+  1 => 
+  array (
+    'children' => 
+    array (
+      0 => 
+      array (
+        'title' => 'Assignment',
+      ),
+      1 => 
+      array (
+        'title' => 'Display',
+      ),
+    ),
+    'title' => 'Variables',
+  ),
+);
 ?>
